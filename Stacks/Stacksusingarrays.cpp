5f1a94b1731cd030632a19cfcc89dfccb15090ad
Sirigirisetty Krishna Vamsi <103875446+KrishnaVamsi-17@users.noreply.github.com>
2024-02-06 10:35:52 +0530

    Create Stacksusingarrays.cpp

---
+++ Stacks/Stacksusingarrays.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Stack{
+   //properties
+   public:
+          int top;
+          int *arr;
+          int size;
+    Stack(int size){
+       this->size=size;
+       arr = new int[size];
+       top=-1;
+    }
+    void push(int data){
+      if(size-top>1){
+        top++;
+        arr[top] = data;
+      }
+      else cout<<"Stack Overflow"<<endl;
+
+    }
+    void pop(){
+       if(top>=0) top--;
+       else cout<<"Stack UnderFlow"<<endl;
+
+    }
+    int peek(){
+      if(top>=0 && top<size) return arr[top];
+      else{
+        cout<<"Stack is empty"<<endl;
+        return -1;
+      }
+    }
+
+    bool isEmpty(){
+      if(top==-1) return true;
+      else return false;
+
+    }
+
+};
+
+int main(){
+
+  Stack st(5);
+
+  st.push(2);
+  st.push(4);
+  st.push(5);
+  cout<<st.peek()<<endl;
+  st.pop();
+  cout<<st.peek()<<endl;
+}
